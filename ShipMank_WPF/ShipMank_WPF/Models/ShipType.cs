159bac14621c9181cb64acd1812f604f55a168b2
ataliesavista <145782339+ataliesavista@users.noreply.github.com>
2025-11-24 20:23:02 +0700

    feat (neondb): mengintegrasikan aplikasi dengan database

--- ShipMank_WPF/ShipMank_WPF/Models/ShipType.cs
+++ ShipMank_WPF/ShipMank_WPF/Models/ShipType.cs
@@ -9,14 +9,16 @@ namespace ShipMank_WPF.Models
     public class ShipType
     {
         public int TypeID { get; private set; }
-        public ShipTypeEnum TypeName { get; private set; }
+        public string TypeName { get; private set; }
         public string Description { get; set; }
+        public string ImagePath { get; set; }
 
-        public ShipType(int typeID, ShipTypeEnum typeName, string description)
+        public ShipType(int typeID, string typeName, string description, string imagePath)
         {
             TypeID = typeID;
             TypeName = typeName;
             Description = description;
+            ImagePath = imagePath;
         }
 
         public override string ToString()
